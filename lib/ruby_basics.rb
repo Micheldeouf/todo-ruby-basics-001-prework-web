5b1d4369da3335b7596ab15fcba444c5f746aaab
Micheldeouf <arnaud.frossard@gmail.com>
2015-10-20 10:12:54 +0200

    Todo Ruby

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+	result = num1 / num2
+	result 
 end
 
 def assign_variable(value)
-  
+  	name = value
+	name
 end
 
-def argue
-  
+def argue(arg1)
+	arg1 = "I'm right and you are wrong!"
+	arg1  
 end
 
-def greeting
-
+def greeting(phrase, person)
+	phrase = "Hello"
+	puts phrase + person
 end
 
-def return_a_value
+def return_a_value(ville="Nice")
 
+	ville
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(repas="expert")
+	repas
 end
 
-def pizza_party
-
+def pizza_party(avec="cheese")
+	avec
 end
